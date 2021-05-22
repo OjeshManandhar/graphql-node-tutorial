8daa7cc3d6d4781e5e49b8107163957829ddfb26
OjeshManandhar <ozes.manandhar@gmail.com>
2021-05-22 12:55:57 +0545

    Use context to access prisma
    Store data in DB

--- src/index.js
+++ src/index.js
@@ -1,59 +1,58 @@
 const fs = require('fs');
 const path = require('path');
 const { ApolloServer } = require('apollo-server');
+const { PrismaClient } = require('@prisma/client');
 
-let links = [
-  {
-    id: 'link-0',
-    url: 'www.howtographql.com',
-    description: 'Fullstack tutorial for GraphQL'
-  }
-];
-let idCount = links.length;
+const prisma = new PrismaClient();
 
 const resolvers = {
   Query: {
     info: () => `This is the API of a Hackernews Clone`,
-    feed: () => links,
-    link: (parent, args) => {
-      return links.find(item => item.id === args.id);
+    feed: (parent, args, context) => {
+      return context.prisma.link.findMany();
     }
+    // link: (parent, args, context) => {
+    //   return links.find(item => item.id === args.id);
+    // }
   },
   Mutation: {
-    post: (parent, args) => {
-      const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
-        url: args.url
-      };
-      links.push(link);
-      return link;
-    },
-    updateLink: (parent, args) => {
-      const index = links.findIndex(item => item.id === args.id);
-
-      if (index !== -1) {
-        links[index].url = args.url;
-        links[index].description = args.description;
-
-        return links[index];
-      }
-    },
-    deleteLink: (parent, args) => {
-      const index = links.findIndex(item => item.id === args.id);
-
-      if (index !== -1) {
-        const link = links[index];
-        links.splice(index, 1);
-        return link;
-      }
+    post: (parent, args, context, info) => {
+      const newLink = context.prisma.link.create({
+        data: {
+          url: args.url,
+          description: args.description
+        }
+      });
+      return newLink;
     }
+    // updateLink: (parent, args) => {
+    //   const index = links.findIndex(item => item.id === args.id);
+
+    //   if (index !== -1) {
+    //     links[index].url = args.url;
+    //     links[index].description = args.description;
+
+    //     return links[index];
+    //   }
+    // },
+    // deleteLink: (parent, args) => {
+    //   const index = links.findIndex(item => item.id === args.id);
+
+    //   if (index !== -1) {
+    //     const link = links[index];
+    //     links.splice(index, 1);
+    //     return link;
+    //   }
+    // }
   }
 };
 
 const server = new ApolloServer({
   typeDefs: fs.readFileSync(path.join(__dirname, 'schema.graphql'), 'utf8'),
-  resolvers
+  resolvers,
+  context: {
+    prisma
+  }
 });
 
 server.listen().then(({ url }) => console.log(`Server is running on ${url}`));
